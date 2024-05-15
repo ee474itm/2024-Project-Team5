151cc84febeb39383c537df23cdc2dc8f26ed275
klae01 <tspt2479@gmail.com>
2024-05-15 22:47:26 +0900

    feat: Optimize memory usage and GPU loading for LLM
    - Modified LLMConversation to prevent duplicate model loading, reducing memory usage
    - Integrated GPU support for model loading to enhance performance
    - Adjusted model initialization to use torch and transformers directly
    - Updated text generation pipeline configurations for improved control
    - Ensured summarizer and main LLM use the same model instance to save memory

--- storystream/llm/conversation.py
+++ storystream/llm/conversation.py
@@ -1,30 +1,44 @@
+import torch
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationSummaryMemory
 from langchain_community.llms import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 
 class LLMConversation:
-    def __init__(self, model_id="meta-llama/Meta-Llama-3-8B", summarizer=False):
-        self.llm = HuggingFacePipeline.from_model_id(
-            model_id=model_id,
-            task="text-generation",
-            pipeline_kwargs={
-                "max_new_tokens": 512,
-                "top_k": 30,
-                "temperature": 0.7,
-                "repetition_penalty": 1.03,
-            },
+    def __init__(
+        self, model_id="meta-llama/Meta-Llama-3-8B-Instruct", summarizer=False
+    ):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model = AutoModelForCausalLM.from_pretrained(
+            model_id, torch_dtype=torch.bfloat16
         )
-        self.summarizer = HuggingFacePipeline.from_model_id(
-            model_id=model_id,
-            task="text-generation",
-            pipeline_kwargs={
-                "max_new_tokens": 512,
-                "temperature": 0.1,
-                "repetition_penalty": 1.03,
-            },
-        )
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
 
+        self.llm = HuggingFacePipeline(
+            pipeline=pipeline(
+                "text-generation",
+                model=model,
+                tokenizer=tokenizer,
+                device=device,
+                max_new_tokens=512,
+                temperature=0.7,
+                repetition_penalty=1.03,
+                do_sample=True,
+            )
+        )
+        self.summarizer = HuggingFacePipeline(
+            pipeline=pipeline(
+                "text-generation",
+                model=model,
+                tokenizer=tokenizer,
+                device=device,
+                max_new_tokens=512,
+                temperature=0.1,
+                repetition_penalty=1.03,
+                do_sample=False,
+            )
+        )
         if summarizer:
             self.conversation_chain = ConversationChain(
                 llm=self.llm,
