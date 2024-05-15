3a7d6c8987ea0056de95d3ddbe75c70e52358b47
jhkjames1110 <jhk.james1110@gmail.com>
2024-05-16 04:02:59 +0900

    add classification model code

--- storystream/audio/classification.py
+++ storystream/audio/classification.py
@@ -1,8 +1,47 @@
+from storystream.audio.model import M2EClassifier
+import torch
+import numpy as np
+import librosa
+
 class AudioClassifier:
-    def __init__(self):
-        # Load your audio classification model here
+    def __init__(self, model_path, device):
+        self.model = M2EClassifier()
+        self.device = device
+        self.emotions = ['romantic', 'aggressive', 'sad', 'happy', 'dramatic']
+        
+        self.model.load_state_dict(torch.load(model_path))
+        self.model.eval()
+        self.model.to(self.device)        
         pass
+    
+    def extract_features(self, data, sample_rate):
+        result = np.array([])
+        zcr = np.mean(librosa.feature.zero_crossing_rate(y=data, hop_length=20).T, axis=0)
+        result = np.hstack((result, zcr))
+        
+        stft = np.abs(librosa.stft(data))
+        chroma_stft = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate, n_fft=20, hop_length=20).T, axis=0)
+        result = np.hstack((result, chroma_stft))
+        
+        mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate, n_mfcc=20).T, axis=0)
+        result = np.hstack((result, mfcc))
+        
+        rms = np.mean(librosa.feature.rms(y=data, frame_length=100).T, axis=0)
+        result = np.hstack((result, rms))
+        
+        mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sample_rate, hop_length=20).T, axis=0)
+        result = np.hstack((result, mel))
+        
+        return result
+    
+    def feature_extractor(self, path):
+        data, sample_rate = librosa.load(path)
+        res1 = self.extract_features(data, sample_rate)
+        result = np.array(res1)
+        return result
 
     def classify(self, audio_file_path):
-        # Implement audio classification logic
-        return "classified_audio_info"
+        feature = self.feature_extractor(audio_file_path)
+        feature = torch.tensor(feature, dtype=torch.float32).to(self.device)
+        _, predicted_idx = torch.max(self.model(feature), 1)
+        return self.emotions[predicted_idx]
--- storystream/audio/context_extraction.py
+++ storystream/audio/context_extraction.py
@@ -2,8 +2,8 @@ from storystream.audio.classification import AudioClassifier
 
 
 class AudioContextExtractor:
-    def __init__(self):
-        self.classifier = AudioClassifier()
+    def __init__(self, model_path, device):
+        self.classifier = AudioClassifier(model_path=model_path, device=device)
 
     def extract_context(self, audio_file_path):
         classified_info = self.classifier.classify(audio_file_path)
